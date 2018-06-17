6767b8a56f6a46308a99fdd6fe81a7f15ce5b8b7
DENISSA-PC\Sergey <deniskuranov@mail.ru>
2018-06-17 08:03:44 +0400

    BT_0.0.1

---
+++ BTanks/Source/BTanks/BTanks.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BTanks, "BTanks" );
